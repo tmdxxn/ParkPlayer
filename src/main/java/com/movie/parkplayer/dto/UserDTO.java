529700b3702f2eb255835a5990cdb37bc6e204c5
tmdxxn <157607304+tmdxxn@users.noreply.github.com>
2024-06-17 18:02:42 +0900

    헤더 지우기

--- src/main/java/com/movie/parkplayer/dto/UserDTO.java
+++ src/main/java/com/movie/parkplayer/dto/UserDTO.java
@@ -1,46 +1,6 @@
 package com.movie.parkplayer.dto;
 
 import com.movie.parkplayer.entity.UserEntity;
-<<<<<<< HEAD
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserDTO {
-
-    private Long id;
-
-    private String userId;
-
-    private String userPw;
-
-    private String userName;
-
-    private String userEmail;
-
-    private LocalDate userBirth;
-
-    private String userTell;
-
-    // DTO -> Entity 변환
-    public UserEntity toEntity() {
-        return UserEntity.builder()
-                .id(id)
-                .userId(userId)
-                .userName(userName)
-                .userEmail(userEmail)
-                .userBirth(userBirth)
-                .userTell(userTell)
-                .build();
-    }
-
-
-=======
 import com.movie.parkplayer.repository.UserRole;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -75,5 +35,4 @@ public class UserDTO {
                 .role(role)
                 .build();
     }
->>>>>>> 3f118e6b05b4f0920fdcd4a35fb4d0f1665c9345
 }
