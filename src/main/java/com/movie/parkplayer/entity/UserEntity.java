9db242845fd770a6c6992791b06571de3f61b7bd
tmdxxn <157607304+tmdxxn@users.noreply.github.com>
2024-06-17 18:03:09 +0900

    헤더 지우기

--- src/main/java/com/movie/parkplayer/entity/UserEntity.java
+++ src/main/java/com/movie/parkplayer/entity/UserEntity.java
@@ -1,52 +1,5 @@
 package com.movie.parkplayer.entity;
 
-<<<<<<< HEAD
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.time.LocalDate;
-
-@Entity
-@Table(name = "member")
-@Getter // 게터 사용해서 모든 필드 접근 가능하게끔.. 세터는 보안위험성때문에 제외
-@ToString
-@NoArgsConstructor
-public class UserEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, unique = true)
-    private String userId;
-
-    @Column(nullable = false)
-    private String userPw;
-
-    @Column(nullable = false)
-    private String userName;
-
-    @Column(nullable = false)
-    private String userEmail;
-
-    @Column(nullable = false)
-    private LocalDate userBirth;
-
-    @Column(nullable = false)
-    private String userTell;
-
-
-     // 기본 생성자 (빌더패턴을 사용해서 직관적으로 볼수있게 만듦)
-    @Builder
-    public UserEntity(Long id, String userId, String userPw, String userName, String userEmail, LocalDate userBirth, String userTell) {
-        this.id = id;
-        this.userId = userId;
-        this.userPw = userPw;
-        this.userName = userName;
-        this.userEmail = userEmail;
-        this.userBirth = userBirth;
-        this.userTell = userTell;
-=======
 import com.movie.parkplayer.repository.UserRole;
 import jakarta.persistence.*;
 import lombok.Builder;
@@ -110,6 +63,5 @@ public class UserEntity implements Serializable {
 
     public void changePassword(String newPassword) {
         this.memPassword = newPassword;
->>>>>>> 3f118e6b05b4f0920fdcd4a35fb4d0f1665c9345
     }
 }
