d915c3a1b3456c3cea28d14b699b4825203f9dc2
tmdxxn <157607304+tmdxxn@users.noreply.github.com>
2024-06-17 18:01:36 +0900

    헤드지우기..

--- src/main/java/com/movie/parkplayer/controller/HomeController.java
+++ src/main/java/com/movie/parkplayer/controller/HomeController.java
@@ -1,17 +1,5 @@
 package com.movie.parkplayer.controller;
 
-<<<<<<< HEAD
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class HomeController {
-
-    @GetMapping("/")
-    public String home() {
-        return "메인화면 만들어지면 그거 넣으면 됨.";
-    }
-=======
 import com.movie.parkplayer.entity.Notice;
 import com.movie.parkplayer.service.NoticeService;
 import lombok.extern.slf4j.Slf4j;
